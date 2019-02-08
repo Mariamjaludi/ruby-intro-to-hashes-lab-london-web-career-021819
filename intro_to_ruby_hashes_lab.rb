2a3d202bd272caed800ce86ded2aa14ede2083ae
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-08 08:16:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,9 +8,10 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
-	monopoly[:railroads] = {}
-	return monopoly[:railroads]
+	monopoly = {
+		railroads = {}
+	}
+	
 end
 
 def monopoly_with_second_tier
